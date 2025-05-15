7db5510d8fef8490ae34e4dfdccb240031993266
Joao Paulo <joao_paulo_oandrade@hotmail.com>
2025-05-15 20:35:03 -0300

    clientes completo

--- lib/views/clientes.dart
+++ lib/views/clientes.dart
@@ -12,9 +12,10 @@ class _ClientesState extends State<Clientes> {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(backgroundColor: const Color.fromARGB(255, 170, 233, 53),
                      title: Text("Clientes",style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),),
-                     )
+                     ),
+                     body: SingleChildScrollView(child: Container(padding: EdgeInsets.all(20),child: Column(children: [Row(children: [Image.asset("imagens/detalhe_cliente.png"),Text("Clientes",)],),SizedBox(height: 20,),Row(children: [Image.asset("imagens/cliente1.png")],),SizedBox(height: 20,),Row(children: [Text("Empresa de software")],),SizedBox(height: 20,),Row(children: [Image.asset("imagens/cliente2.png")],),SizedBox(height: 20,),Row(children: [Text("Empresa de auditoria")],)],),),) ,
                      
                      
-                     ,);
+                     );
   }
 }
--- lib/views/contato.dart
+++ lib/views/contato.dart
@@ -12,9 +12,10 @@ class _ContatoState extends State<Contato> {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(backgroundColor: Colors.green,
                      title: Text("Contatos",style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),),
-                     )
+                     ),
+                     body: SingleChildScrollView(child: Container(padding: EdgeInsets.all(20),child: Column(children: [Row(children: [Image.asset("imagens/detalhe_contato.png"),Text("Contatos")],),SizedBox(height: 20,),Row(children: [Text("Atendimento@atm.com.br")],),SizedBox(height: 20,),Row(children: [Text("11-4087-9090")],),SizedBox(height: 20,),Row(children: [Text("11-99999-9090")],)],),),) ,
                      
                      
-                     ,);
+                     );
   }
 }
